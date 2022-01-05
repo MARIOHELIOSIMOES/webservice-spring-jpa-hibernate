1f57b461117d7e58d2ba182e573539cbf515af15
MARIOHELIOSIMOES <mariohelio_simoes@hotmail.com>
2022-01-05 20:38:03 -0300

    Project created

---
+++ src/main/java/com/simoes/webspring/WebspringApplication.java
@@ -0,0 +1,13 @@
+package com.simoes.webspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simoes/webspring/WebspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simoes.webspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
