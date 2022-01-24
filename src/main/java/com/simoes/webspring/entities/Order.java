ef2c377d4082749a22362b6cbe6e8a0b27ad6146
MARIOHELIOSIMOES <mariohelio_simoes@hotmail.com>
2022-01-24 19:43:46 -0300

    OrderStatus enum

--- src/main/java/com/simoes/webspring/entities/Order.java
+++ src/main/java/com/simoes/webspring/entities/Order.java
@@ -12,8 +12,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import org.hibernate.cfg.annotations.Nullability;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+import com.simoes.webspring.entities.enums.OrderStatus;
 
 @Entity
 @Table(name="tb_order")
@@ -28,6 +31,8 @@ public class Order implements Serializable{
 	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss'Z'", timezone="GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name="client_id")
 	private User client;
@@ -36,12 +41,23 @@ public class Order implements Serializable{
 	
 	}
 	
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus!=null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
+
 	public Long getId() {
 		return id;
 	}
