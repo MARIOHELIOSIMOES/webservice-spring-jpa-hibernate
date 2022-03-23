23b5e39f0d642c4ba50415b42e70df4453869c73
marioheliosimoes <mariohelio_simoes@hotmail.com>
2022-03-23 14:44:50 -0300

    OrderItem, many-to-many asssociation with extra attributes parte 2

--- src/main/java/com/simoes/webspring/config/TestConfig.java
+++ src/main/java/com/simoes/webspring/config/TestConfig.java
@@ -10,10 +10,13 @@ import org.springframework.context.annotation.Profile;
 
 import com.simoes.webspring.entities.Category;
 import com.simoes.webspring.entities.Order;
+import com.simoes.webspring.entities.OrderItem;
 import com.simoes.webspring.entities.Product;
 import com.simoes.webspring.entities.User;
 import com.simoes.webspring.entities.enums.OrderStatus;
+import com.simoes.webspring.entities.pk.OrderItemPK;
 import com.simoes.webspring.repositories.CategoryRepository;
+import com.simoes.webspring.repositories.OrderItemRepository;
 import com.simoes.webspring.repositories.OrderRepository;
 import com.simoes.webspring.repositories.ProductRepository;
 import com.simoes.webspring.repositories.UserRepository;
@@ -34,6 +37,8 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private ProductRepository productRepository;
 	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -71,6 +76,15 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p4.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p1.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
 	}
 }
--- src/main/java/com/simoes/webspring/entities/OrderItem.java
+++ src/main/java/com/simoes/webspring/entities/OrderItem.java
@@ -7,6 +7,7 @@ import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.simoes.webspring.entities.pk.OrderItemPK;
 
 
@@ -18,7 +19,7 @@ public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@EmbeddedId
-	private OrderItemPK id;
+	private OrderItemPK id = new OrderItemPK();
 	
 	private Integer quantity;
 	private Double price;
@@ -33,6 +34,7 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
