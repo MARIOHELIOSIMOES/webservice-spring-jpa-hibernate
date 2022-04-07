aa4d42850bcc0da3d7a5179cb8ac38cdd831e6c4
Mário Hélio Simões <marioheliosimoes@saaemogi.com.br>
2022-04-07 15:08:35 -0300

    User insert

--- src/main/java/com/simoes/webspring/resources/UserResource.java
+++ src/main/java/com/simoes/webspring/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.simoes.webspring.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.simoes.webspring.entities.User;
 import com.simoes.webspring.services.UserService;
@@ -29,6 +33,17 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User user = userService.findById(id);
 		return ResponseEntity.ok().body(user);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(obj.getId())
+				.toUri();
+		
+		return ResponseEntity.created(uri).body(obj);
 	}
 
 }
--- src/main/java/com/simoes/webspring/services/UserService.java
+++ src/main/java/com/simoes/webspring/services/UserService.java
@@ -25,4 +25,8 @@ public class UserService {
 		return user.get();
 	}
 	
+	public User insert(User obj) {
+		return userRepository.save(obj);
+	}
+	
 }
