d06368e636f2eec72e7ee7c6d007fb73115b5d72
Mário Hélio Simões <marioheliosimoes@saaemogi.com.br>
2022-04-08 16:45:42 -0300

    Exception Handling - findById

--- src/main/java/com/simoes/webspring/services/UserService.java
+++ src/main/java/com/simoes/webspring/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.simoes.webspring.entities.User;
 import com.simoes.webspring.repositories.UserRepository;
+import com.simoes.webspring.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> user = userRepository.findById(id);
-		return user.get();
+		return user.orElseThrow( () -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
