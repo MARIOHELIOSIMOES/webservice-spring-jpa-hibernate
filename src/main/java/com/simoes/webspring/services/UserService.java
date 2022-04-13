3ed794b884fd4bba662c7458a444ad26c71e00e2
Mário Hélio Simões <marioheliosimoes@saaemogi.com.br>
2022-04-13 14:08:53 -0300

    Exception Handleing - delete

--- src/main/java/com/simoes/webspring/services/UserService.java
+++ src/main/java/com/simoes/webspring/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.simoes.webspring.entities.User;
 import com.simoes.webspring.repositories.UserRepository;
+import com.simoes.webspring.services.exceptions.DatabaseException;
 import com.simoes.webspring.services.exceptions.ResourceNotFoundException;
 
 
@@ -31,7 +34,14 @@ public class UserService {
 	}
 	
 	public void delete (Long id) {
-		userRepository.deleteById(id);
+		try {
+			userRepository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User update(Long id, User obj) {
